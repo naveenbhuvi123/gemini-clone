3870304e7ac7582027644e281ab15ddc4cf5ae70
naveenbhuvi123 <156157228+naveenbhuvi123@users.noreply.github.com>
2024-09-22 18:34:01 +0530

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
-import { Context } from '../../context/Context'
+import { Context } from '../../context/Context.jsx'
 
 const Mane = () => {
 
